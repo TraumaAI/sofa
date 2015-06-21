573d0438b6016b098afec137115f9c092bab3970
Mike <matadorcode@gmail.com>
2015-06-20 23:12:39 -0400

    completed sofa app

--- sofa-app/server.R
+++ sofa-app/server.R
@@ -7,17 +7,90 @@
 
 library(shiny)
 
+resp_list <- c(">= 400", "300-399", "200-299", "100-199", "<= 99")
+vent_list <- c("No", "Yes")
+gcs_list <- c("15", "13-14", "10-12", "6-9", "< 6")
+cv_list <- c("MAP > 70", "MAP <= 70", "Dopamine <= 5", "Dopamine > 5, Epinephrine/Norepinephrine <= 0.1", "Dopamine > 15, Epinephrine/Norepinephrine > 0.1")
+liver_list <- c("<= 1.1", "1.2 - 1.9", "2.0 - 5.9", "6.0 - 11.9", ">= 12")
+coag_list <- c(">= 150", "100 - 149", "50 - 99", "20-49", "<= 19")
+renal_list <- c("<=1.1", "1.2-1.9", "2.0-3.4", "3.5-4.9", ">= 5.0")
+uop_list <- c("> 500", "200 - 500", "< 200")
+
 shinyServer(function(input, output) {
 
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-  })
+  output$resp_value <- renderPrint({ 
+          if (input$respiratory == 0) return(resp_list[1])
+          if (input$respiratory == 1) return(resp_list[2])
+          if (input$respiratory == 2) return(resp_list[3])
+          if (input$respiratory == 3) return(resp_list[4])
+          if (input$respiratory == 4) return(resp_list[5])
+          })
+  output$vent_value <-renderPrint({
+          if (input$ventilator == 0) return(vent_list[1])
+          if (input$ventilator == 1) return(vent_list[2])
+          })
+  output$neuro_value <- renderPrint({
+          if (input$neuro == 0) return(gcs_list[1])
+          if (input$neuro == 1) return(gcs_list[2])
+          if (input$neuro == 2) return(gcs_list[3])
+          if (input$neuro == 3) return(gcs_list[4])
+          if (input$neuro == 4) return(gcs_list[5])
+          })
+  output$cv_value <- renderPrint({
+          if (input$cv == 0) return(cv_list[1])
+          if (input$cv == 1) return(cv_list[2])
+          if (input$cv == 2) return(cv_list[3])
+          if (input$cv == 3) return(cv_list[4])
+          if (input$cv == 4) return(cv_list[5])
+          })
+  output$liver_value <- renderPrint({
+          if (input$liver == 0) return(liver_list[1])
+          if (input$liver == 1) return(liver_list[2])
+          if (input$liver == 2) return(liver_list[3])
+          if (input$liver == 3) return(liver_list[4])
+          if (input$liver == 4) return(liver_list[5])
+          })
+  output$coag_value <- renderPrint({
+          if (input$coag == 0) return(coag_list[1])
+          if (input$coag == 1) return(coag_list[2])
+          if (input$coag == 2) return(coag_list[3])
+          if (input$coag == 3) return(coag_list[4])
+          if (input$coag == 4) return(coag_list[5])
+          })
+  output$renal_value <- renderPrint({
+          if (input$renal == 0) return(renal_list[1])
+          if (input$renal == 1) return(renal_list[2])
+          if (input$renal == 2) return(renal_list[3])
+          if (input$renal == 3) return(renal_list[4])
+          if (input$renal == 4) return(renal_list[5])
+          })
+  output$uop_value <-renderPrint({
+          input$uop
+          if (input$uop == 0) return(uop_list[1])
+          if (input$uop == 1) return(uop_list[2])
+          if (input$uop == 2) return(uop_list[3])
+          })
+  s <- function(r, v, n, cv, l, cg, cr, up) {
+          sofa = 0
+          if (as.integer(r) <= 2){
+                  sofa = sofa + as.integer(r)
+          } else { 
+                  if (as.integer(v) == 1){
+                          sofa = sofa + as.integer(r)
+                  }else{
+                        sofa = sofa + 2
+                  }
+          }
+          
+          sofa = sofa + as.integer(n) + as.integer(cv) + as.integer(l) + as.integer(cg)
+          
+          if (as.integer(up) >= 1){
+                  if ((as.integer(up) + 2) >= as.integer(cr)){
+                          sofa = sofa + as.integer(up) + 2
+                  }else{sofa = sofa + as.integer(cr)}
+          }else{sofa = sofa + as.integer(cr)}
+  }
+ 
+output$sofa_value <- renderText({s(input$respiratory, input$ventilator, input$neuro, input$cv, input$liver, input$coag, input$renal, input$uop)})
 
 })
--- sofa-app/ui.R
+++ sofa-app/ui.R
@@ -13,18 +13,70 @@ shinyUI(fluidPage(
   titlePanel("Sofa Score"),
 
   # Sidebar with a slider input for number of bins
-  sidebarLayout(
+  sidebarLayout(position = "right",
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+      selectInput("respiratory", label = h4("PaO2 / FiO2 (mmHg)"), 
+                  choices = list(">= 400" = 0, "300 - 399" = 1, "200 - 299" = 2, "100 - 199" = 3, "< 99" = 4), 
+                  selected = 0),
+      radioButtons("ventilator", label = h4("Mechanical Ventilation"),
+                   choices = list("No" = 0, "Yes" = 1), 
+                   selected = 0,
+                   inline = TRUE),
+      selectInput("neuro", label = h4("Glasgow Comma Scale"), 
+                  choices = list("15" = 0, "13-14" = 1, "10-12" = 2, "6-9" = 3, "<6" = 4), 
+                  selected = 0),
+      selectInput("cv", label = h4("Mean Arterial Pressue", "Vasopressors (mcg/kg/min)"), 
+                  choices = list("MAP > 70" = 0, "MAP <= 70" = 1, "Dopamine <= 5" = 2, "Dopamine > 5 OR Epinephrine/Norepinephrine <= 0.1" = 3, "Dopamine > 15 OR Epinephrine/Norepinephrine > 0.1" = 4), 
+                  selected = 0),
+      selectInput("liver", label = h4("Bilirubin"), 
+                  choices = list("<= 1.1" = 0, "1.2 - 1.9" = 1, "2.0 - 5.9" = 2, "6.0 - 11.9" = 3, ">= 12" = 4), 
+                  selected = 0),
+      selectInput("coag", label = h4("Platelets"), 
+                  choices = list(">= 150" = 0, "100 - 149" = 1, "50 - 99" = 2, "20-49" = 3, "<= 19" = 4), 
+                  selected = 0),
+      selectInput("renal", label = h4("Creatinine"), 
+                  choices = list("<=1.1" = 0, "1.2-1.9" = 1, "2.0-3.4" = 2, "3.5-4.9" = 3, ">= 5.0" = 4), 
+                  selected = 0),
+      radioButtons("uop", label = h4("Urine Output (ml/day)"),
+                   choices = list("> 500 " = 0, "200 - 500" = 1, "< 200" = 2), 
+                   selected = 0,
+                   inline = TRUE)
+      
+      # actionButton("goButton", "Go!")
     ),
 
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot")
+      p("The Sequential Organ Failure Assessment (SOFA) is a morbidity severity score and mortality estimation tool.  The parameters found on the right are physiologic variables used to calcute the score.  Essentially, one uses the worst score of a parameter in a 24 hour period."),
+      p("Normal physiologic parameters already pre-selected. The rank order is best to worst on the drop down lists and left to right on the radio button selections"),
+      hr(),
+      fluidRow(column(4, "PaO2/FiO2 ratio:" ), 
+               column(4, "Ventilated:"),
+               column(4, "Glasgow Comma:")
+               ),
+      fluidRow(column(4, verbatimTextOutput("resp_value")),
+               column(4, verbatimTextOutput("vent_value")),
+               column(4, verbatimTextOutput("neuro_value"))
+               ),
+      fluidRow(column(4, "MAP & Pressors:" ), 
+               column(4, "Bilirubin:"),
+               column(4, "Platelets:")
+      ),
+      fluidRow(column(4, verbatimTextOutput("cv_value")),
+               column(4, verbatimTextOutput("liver_value")),
+               column(4, verbatimTextOutput("coag_value"))
+      ),
+      fluidRow(column(4, "Creatinine:" ), 
+               column(4, "Urine Output:")
+      ),
+      fluidRow(column(4, verbatimTextOutput("renal_value")),
+               column(4, verbatimTextOutput("uop_value"))
+      ),
+      hr(),
+      h4("SOFA Score: "),
+      h4(uiOutput("sofa_value")),
+      hr()
+      
     )
   )
 ))
